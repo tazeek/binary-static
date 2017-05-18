f38d7624883d360719fadd1305ed5908f929f1b4
Tazeek <tazeek@binary.com>
2017-05-18 16:35:07 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -36677,7 +36677,7 @@
 	    };
 
 	    var viewButtonOnClick = function viewButtonOnClick(container_selector) {
-	        $(container_selector).on('click', '.ref', function (e) {
+	        $(container_selector).on('click', '.statement_row', function (e) {
 	            e.preventDefault();
 	            init(this);
 	        });
@@ -81772,6 +81772,9 @@
 	        var credit_debit_type = parseFloat(transaction.amount) >= 0 ? 'profit' : 'loss';
 
 	        var $statement_row = Table.createFlexTableRow([statement_data.date, '<span ' + showTooltip(statement_data.app_id, oauth_apps[statement_data.app_id]) + '>' + statement_data.ref + '</span>', statement_data.payout, localize(statement_data.action), '', statement_data.amount, statement_data.balance, ''], columns, 'data');
+
+	        $statement_row.attr('class', 'statement_row');
+	        $statement_row.attr('contract_id', statement_data.id);
 
 	        $statement_row.children('.credit').addClass(credit_debit_type);
 	        $statement_row.children('.date').addClass('pre');
